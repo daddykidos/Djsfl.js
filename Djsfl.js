cb99b9cc723acdf15224961c56aef520605a077b
daddykidos <89294381+daddykidos@users.noreply.github.com>
2025-04-15 19:41:02 +0800

    mainRatio concept applied for position and size of elements

--- Djsfl.js
+++ Djsfl.js
@@ -1,15 +1,18 @@
 /** djsfl.js object properties
- * object which are created with the createElement function has the 'djsflElement' className
- *
- * elementHandler() - function to refresh properties of elements
- *
- * properties:
- * pos - vector2        note: (not position) as conflicts with js
- * size - vector2
- * anchor - vector2     note: default vec2(0,0) - top left
- *
- * mainRatio - vector2  note: keeps ratio of pos/size to match window
- * (may or may not be added - depending on use case)
+  object which are created with the createElement function has the 'djsflElement' className
+
+  elementHandler() - function to refresh properties of elements
+
+  properties:
+  pos - vector2       note: (not position) as conflicts with js
+  size - vector2
+  anchor - vector2    note: default vec2(0,0) - top left
+
+  posRatio - vector2
+  sizeRatio - vector2
+
+  usePosRatio - bool  note: false - pixel pos | true - ratioed pos
+  SizeState - number  note: 0-pixel | 1-width | 2-height | 3-own axis
  */
 
 
@@ -43,15 +46,19 @@ function createElement(type, debug, id) {
     const objS = obj.style;
     obj.className = 'djsflElement';
     if (id != undefined) { obj.id = id };
+
+    //element data
     obj.pos = vec2(); obj.anchor = vec2(); obj.size = vec2()
 
-
     if (debug == true) {
-        objS.position = 'absolute';
-        objS.background = 'grey';
+        Object.assign(objS, {
+            position: 'absolute',
+            background: 'gray',
+            width: obj.size.x + 'px',
+            height: obj.size.y + 'px'
+        });
         obj.size = perWin(.1);
-        objS.width = obj.size.x + 'px';
-        objS.height = obj.size.y + 'px';
+
     }; return document.body.appendChild(obj);
 };
 
@@ -69,9 +76,7 @@ function rltvDisp(x, y) {
 function rgb(r, g, b) { return 'rgb(' + r + ',' + g + ',' + b + ')' }
 
 //return the min
-function minWin() {
-    return Math.min(window.innerHeight, window.innerWidth)
-}
+function minWin() { return Math.min(window.innerHeight, window.innerWidth) };
 
 
 
@@ -83,12 +88,55 @@ function elementHandler() {
     Array.from(document.body.getElementsByClassName('djsflElement')).forEach((obj, index) => {
         let objS = obj.style
 
-        //refreshing size
-        objS.width = obj.size.x + "px"; objS.height = obj.size.y + "px"
+        //element resizing
+        let sizeState = obj.sizeState
+        let sizeRatio = obj.sizeRatio
+        let size = obj.size
+        let windowW = window.innerWidth; windowH = window.innerHeight
 
-        //refreshing position - anchor applied
-        objS.left = obj.pos.x - obj.anchor.x * obj.size.x + 'px'
-        objS.top = obj.pos.y - obj.anchor.y * obj.size.y + 'px'
+        function setSize(x, y) {
+            objS.width = x + "px"
+            objS.height = y + 'px'
+            obj.size = vec2(x, y)
+        }
+
+        if (sizeState == 0 || sizeState == undefined) {
+            //pixel size
+            setSize(size.x, size.y)
+
+        } else if (sizeState == 1) {
+            //width ratio size
+            setSize(sizeRatio.x * windowW, sizeRatio.y * windowW)
+
+        } else if (sizeState == 2) {
+            //heigh ratio size
+            setSize(sizeRatio.x * windowH, sizeRatio.y * windowH)
+
+        } else if (sizeState == 3) {
+            //own axis ratio size
+            setSize(sizeRatio.x * windowW, sizeRatio.y * windowH)
+
+        }
+
+
+
+        function setPos(x, y) {
+            obj.pos = vec2(x, y)
+            objS.left = x + 'px'
+            objS.top = y + 'px'
+        }
+
+        //positioning - anchor applied
+        let anchorX = obj.anchor.x * obj.size.x
+        let anchorY = obj.anchor.y * obj.size.y
+        if (obj.usePosRatio == false || obj.usePosRatio == undefined) {
+            //pixel position
+            setPos(obj.pos.x - anchorX, obj.pos.y - anchorY)
+
+        } else {
+            //ratioed position
+            setPos(obj.posRatio.x * windowW - anchorX, obj.posRatio.y * windowH - anchorY)
+        }
 
     })
 };
